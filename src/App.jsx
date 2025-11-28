6bf112158a876fca93f1da65e409f5aab91f0209
sheikh4200 <sheikhrizwanghulamnabi555@gmail.com>
2025-11-28 12:23:35 +0500

    add welcome component for  portfolio animation and use GSAP for animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import NavBar from "#components/NavBar"
+import { NavBar, Welcome } from "#components"
+
 
 const App = () => {
   return (
     <main >
       <NavBar/>
+      <Welcome/>
     </main>
   )
 }
